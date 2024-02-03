24d3e2caac60481d1932bc778307ff532c8b751f
Naveen <nasharma314@gmail.com>
2024-02-03 22:48:26 +0530

    Day_01

---
+++ 100 Days Coding Challenge/Day_01.java
@@ -0,0 +1,39 @@
+/* Aim : An integer has sequential digits if and only if each digit in the number 
+        is one more than the previous digit.
+    Return a sorted list of all the integers in the range [low, high] inclusive 
+        that have sequential digits.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day_01 {
+
+    public static List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ans = new ArrayList<>();
+
+        for (int digit = 1; digit <= 9; digit++) {
+            int current = digit;
+            int next = current + 1;
+
+            while (next <= 9 && current <= high) {
+                current = current * 10 + next;
+
+                if (current >= low && current <= high) {
+                    ans.add(current);
+                }
+
+                next++;
+            }
+        }
+
+        ans.sort(null);
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int low = 1000, high = 13000;
+        System.out.println(sequentialDigits(low, high));
+    }
+}
