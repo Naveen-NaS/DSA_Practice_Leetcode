901a1530b787350899a20dd8f15952af2e49d7ae
Naveen <nasharma314@gmail.com>
2024-02-06 23:59:51 +0530

    06/02/2024

---
+++ String/Problem_49_Feb06.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Problem_49_Feb06 {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> strMap = new HashMap<>();
+        int len = strs.length;
+
+        for (int i = 0; i < len; i++) {
+            char[] charArray = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!strMap.containsKey(sortedStr)) {
+                strMap.put(sortedStr, new ArrayList<>());
+            }
+
+            strMap.get(sortedStr).add(strs[i]);
+        }
+
+        return new ArrayList<>(strMap.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        System.out.println(groupAnagrams(strs));
+    }
+}
