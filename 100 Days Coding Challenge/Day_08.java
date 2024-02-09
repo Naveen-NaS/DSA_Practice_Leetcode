45ca73440c1a9bd9c61a8fc7cc34f8f9105176d7
Naveen <nasharma314@gmail.com>
2024-02-09 08:13:07 +0530

    Day_08

---
+++ 100 Days Coding Challenge/Day_08.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Day_08 {
+
+    public static List<Integer> largestDivisibleSubset(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+
+        int maxSize = 1;
+        int maxIndex = 0;
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if ((nums[i] % nums[j]) == 0) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            if (dp[i] > maxSize) {
+                maxSize = dp[i];
+                maxIndex = i;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int size = maxSize;
+        int index = maxIndex;
+        int lastValue = nums[index];
+
+        for (int i = maxIndex; i >= 0; i--) {
+            if (size == dp[i] && lastValue % nums[i] == 0) {
+                ans.add(nums[i]);
+                size--;
+                lastValue = nums[i];
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 5, 2, 7, 9, 3, 4, 5 };
+        System.out.println(largestDivisibleSubset(nums));
+    }
+}
