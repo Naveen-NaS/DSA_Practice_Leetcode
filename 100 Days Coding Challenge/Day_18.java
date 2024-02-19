1c48a12659cc6a057c483e92001bea34fdca6b2a
Naveen <nasharma314@gmail.com>
2024-02-19 23:03:07 +0530

    Day_18

---
+++ 100 Days Coding Challenge/Day_18.java
@@ -0,0 +1,13 @@
+public class Day_18 {
+
+    static boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(16));
+    }
+}
