c4f8649ccbf7e98f765d0293b87ddda9d1a10502
Naveen <nasharma314@gmail.com>
2024-03-06 22:48:59 +0530

    Day_34

---
+++ 100 Days Coding Challenge/Day_34.java
@@ -0,0 +1,66 @@
+public class Day_34 {
+
+    static class ListNode {
+        int data;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkedList {
+        ListNode head;
+
+        void addNode(int data) {
+            ListNode newNode = new ListNode(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                ListNode current = head;
+                while (current.next != null) {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+
+        void displayList() {
+            ListNode current = head;
+            while (current != null) {
+                System.out.print(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        for (int i = 1; i <= 5; i++) {
+            list.addNode(i);
+        }
+
+        System.out.println(hasCycle(list.head));
+    }
+}
