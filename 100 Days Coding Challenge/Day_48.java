c557001f91e0f14f53047aafd4b850df4146a676
Naveen <nasharma314@gmail.com>
2024-03-21 06:06:32 +0530

    100DaysChallenge

---
+++ 100 Days Coding Challenge/Day_48.java
@@ -0,0 +1,42 @@
+public class Day_48 {
+
+    static class ListNode {
+        int data;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkedList {
+        ListNode head;
+
+        void addNode(int data) {
+            ListNode newNode = new ListNode(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                ListNode current = head;
+                while (current.next != null) {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+
+        void displayList() {
+            ListNode current = head;
+            while (current != null) {
+                System.out.print(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 100 Days Coding Challenge/Day_49.java
@@ -0,0 +1,67 @@
+public class Day_49 {
+
+    static class ListNode {
+        int data;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkedList {
+        ListNode head;
+
+        void addNode(int data) {
+            ListNode newNode = new ListNode(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                ListNode current = head;
+                while (current.next != null) {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+
+        void displayList() {
+            ListNode current = head;
+            while (current != null) {
+                System.out.print(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        for (int i = 1; i <= 5; i++) {
+            list.addNode(i);
+        }
+
+        System.out.println("Original list:");
+        list.displayList();
+
+        list.head = reverseList(list.head);
+        System.out.println("List after swapping first and last nodes:");
+        list.displayList();
+    }
+}
