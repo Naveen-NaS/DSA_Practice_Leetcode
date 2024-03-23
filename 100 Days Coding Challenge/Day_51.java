5045b360282f2a6a8e814e6fa0922ff61ad87c5f
Naveen <nasharma314@gmail.com>
2024-03-23 11:06:35 +0530

    100DaysChallenge

---
+++ 100 Days Coding Challenge/Day_51.java
@@ -0,0 +1,95 @@
+public class Day_51 {
+
+    static class ListNode {
+        int data;
+        ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkedList {
+        ListNode head;
+
+        void addNode(int data) {
+            ListNode newNode = new ListNode(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                ListNode current = head;
+                while (current.next != null) {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+
+        void displayList() {
+            ListNode current = head;
+            while (current != null) {
+                System.out.print(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverse(slow.next);
+        slow.next = null;
+
+        ListNode p1 = head;
+        ListNode p2 = secondHalf;
+        while (p2 != null) {
+            ListNode temp1 = p1.next;
+            ListNode temp2 = p2.next;
+            p1.next = p2;
+            p2.next = temp1;
+            p1 = temp1;
+            p2 = temp2;
+        }
+    }
+
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        for (int i = 1; i <= 6; i++) {
+            list.addNode(i);
+        }
+
+        System.out.print("Original list : ");
+        list.displayList();
+
+        reorderList(list.head);
+        System.out.print("Reordered List : ");
+        list.displayList();
+    }
+}
