8a702a105735fe0e360deca2995a9d35c1321222
Naveen <nasharma314@gmail.com>
2024-04-01 07:04:14 +0530

    Day_15

--- 100 Days Coding Challenge/Day_15.java
+++ 100 Days Coding Challenge/Day_15.java
@@ -36,7 +36,7 @@ public class Day_15 {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 4, 3, 1, 1, 3, 3, 2 };
+        int[] arr = { 4, 3, 1, 2, 3, 3, 2 };
         int k = 3;
         System.out.println(findLeastNumOfUniqueInts(arr, k));
     }
