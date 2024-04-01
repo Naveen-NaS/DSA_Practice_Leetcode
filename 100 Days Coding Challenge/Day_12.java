a5dc09081f386fb7c9c6eded8dc2ffb31d0657d9
Naveen <nasharma314@gmail.com>
2024-04-01 07:02:48 +0530

    Day_12

--- 100 Days Coding Challenge/Day_12.java
+++ 100 Days Coding Challenge/Day_12.java
@@ -28,7 +28,7 @@ public class Day_12 {
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 1, 2 };
+        int[] nums = { 1, 1, 2, 3 };
         System.out.println(largestPerimeter(nums));
     }
 }
