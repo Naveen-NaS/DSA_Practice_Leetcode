511adfa712e5e17bb8e5c6f442a0e900bcc61cd9
Naveen <nasharma314@gmail.com>
2024-04-02 23:29:56 +0530

    Day_21

--- 100 Days Coding Challenge/Day_21.java
+++ 100 Days Coding Challenge/Day_21.java
@@ -37,7 +37,7 @@ public class Day_21 {
 
     public static void main(String[] args) {
         int n = 3;
-        int[][] trust = { { 1, 3 }, { 2, 3 }, { 3, 1 } };
+        int[][] trust = { { 1, 3 }, { 1, 3 }, { 3, 1 } };
         System.out.println(findJudge(n, trust));
     }
 }
