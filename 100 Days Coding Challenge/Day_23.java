17285be0407108cfa925f5351a1cc72feb1a0fe1
Naveen <nasharma314@gmail.com>
2024-04-02 23:31:08 +0530

    Day_23

--- 100 Days Coding Challenge/Day_23.java
+++ 100 Days Coding Challenge/Day_23.java
@@ -88,7 +88,7 @@ public class Day_23 {
 
     public static void main(String[] args) {
         int n = 5, firstPerson = 1;
-        int[][] meetings = { { 3, 4, 2 }, { 1, 2, 1 }, { 2, 3, 1 } };
+        int[][] meetings = { { 3, 4, 1 }, { 1, 2, 1 }, { 2, 3, 1 } };
         System.out.println(findAllPeople(n, meetings, firstPerson));
     }
 }
