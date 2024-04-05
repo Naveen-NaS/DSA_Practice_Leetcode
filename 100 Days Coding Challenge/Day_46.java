7939fb925c8fc47b61da127c372fe336a0275898
Naveen <nasharma314@gmail.com>
2024-04-05 11:33:20 +0530

    Day_46

--- 100 Days Coding Challenge/Day_46.java
+++ 100 Days Coding Challenge/Day_46.java
@@ -38,7 +38,7 @@ public class Day_46 {
     }
 
     public static void main(String[] args) {
-        int[][] points = { { 10, 16 }, { 2, 18 }, { 1, 6 }, { 7, 12 } };
+        int[][] points = { { 10, 16 }, { 2, 8 }, { 1, 6 }, { 7, 12 } };
         System.out.println(findMinArrowShots(points));
     }
 }
