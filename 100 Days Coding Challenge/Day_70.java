ae230cc96b20cffe6c230d7602b05a726450669c
Naveen <nasharma314@gmail.com>
2024-04-10 23:32:29 +0530

    Day_70

--- 100 Days Coding Challenge/Day_70.java
+++ 100 Days Coding Challenge/Day_70.java
@@ -1,3 +1,26 @@
+import java.util.*;
+
 public class Day_70 {
-    
+
+    public static int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int len = deck.length;
+        Deque<Integer> index = new LinkedList<>();
+        for (int i = 0; i < len; i++) {
+            index.offer(i);
+        }
+        int[] result = new int[len];
+        for (int card : deck) {
+            result[index.pollFirst()] = card;
+            if (!index.isEmpty()) {
+                index.offer(index.pollFirst());
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
